207e6a3b5844a4e2a330c32b5647357d044ed907
Ranu <ranuk@packt.com>
2019-10-31 10:20:54 +0530

    code updated

--- Chapter01/Exercise 1.01/src/App.js
+++ Chapter01/Exercise 1.01/src/App.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
- 
-const Greeting = props => <p>Hello {props.name}!</p>;
- 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.title = 'React App';
-    this.state = { clickCounter: 0 };
-  }
-  renderClickCount() {
-    return <p>I've been clicked {this.state.clickCounter} times!</p>;
-  }
-  render() {
-    return (
-      <div>
-        <h1>{this.title}</h1>
-        <br />
-        <Greeting name="User" />
-        <br />
-        {this.renderClickCount()}
-      </div>
-    );
-  }
-}
- 
-export default App;
+import React from 'react';
 
+class App extends React.Component {
+   render() {
+      return (
+         <div className=”Example” id=”my-element”>
+</div>
+<div
+  className="greeting"
+  style={{ background: “black”, color: “white” }}
+  onClick={() => alert('hello')}
+>
+Hello World
+</div>
+
+      );
+   }
+}
+export default App;
